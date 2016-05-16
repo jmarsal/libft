c53db85dc3947a86b76da4556f20674b53ed3eb9
jmarsal <jb.marsal@gmail.com>
2016-05-16 21:39:33 +0200

    push

--- ft_abs.c
+++ ft_abs.c
@@ -6,11 +6,16 @@
 /*   By: jmarsal <jmarsal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/25 16:43:58 by jmarsal           #+#    #+#             */
-/*   Updated: 2016/05/02 11:26:07 by jmarsal          ###   ########.fr       */
+/*   Updated: 2016/05/16 21:38:32 by jmarsal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** n a positive or negative value.
+**Return the absolute value of n.
+*/
 
 int		ft_abs(int n)
 {
