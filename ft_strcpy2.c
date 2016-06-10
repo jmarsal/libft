f6d2280aaa1c32b52985085519c293d06b1d9e6e
jmarsal <jmarsal@student.42.fr>
2016-06-11 01:08:53 +0200

    juste pour essai

--- ft_strcpy2.c
+++ ft_strcpy2.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*   ft_strcpy2.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmarsal <jmarsal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/09 23:31:37 by jmarsal           #+#    #+#             */
-/*   Updated: 2016/06/11 01:07:31 by jmarsal          ###   ########.fr       */
+/*   Updated: 2016/06/11 01:06:42 by jmarsal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,56 @@ static size_t	how_many_words(char const *s, char c)
 	return (j);
 }
 
+static void		ft_strsplit_bulk16(size_t *len, size_t *pointeur_i,
+												char const *s, char *dst)
+{
+	size_t	i;
+
+	i = 0;
+	while (pointeur_i + 16 < len)
+	{
+		dst[i] = s[*pointeur_i];
+		dst[i + 1] = s[*pointeur_i + 1];
+		dst[i + 2] = s[*pointeur_i + 2];
+		dst[i + 3] = s[*pointeur_i + 3];
+		dst[i + 4] = s[*pointeur_i + 4];
+		dst[i + 5] = s[*pointeur_i + 5];
+		dst[i + 6] = s[*pointeur_i + 6];
+		dst[i + 7] = s[*pointeur_i + 7];
+		dst[i + 8] = s[*pointeur_i + 8];
+		dst[i + 9] = s[*pointeur_i + 9];
+		dst[i + 10] = s[*pointeur_i + 10];
+		dst[i + 11] = s[*pointeur_i + 11];
+		dst[i + 12] = s[*pointeur_i + 12];
+		dst[i + 13] = s[*pointeur_i + 13];
+		dst[i + 14] = s[*pointeur_i + 14];
+		dst[i + 15] = s[*pointeur_i + 15];
+		i += 16;
+		*pointeur_i += 16;
+	}
+}
+
+static void		ft_strsplit_bulk8(size_t *len, size_t *pointeur_i,
+												char const *s, char *dst)
+{
+	size_t	i;
+
+	i = 0;
+	while (pointeur_i + 8 < len)
+	{
+		dst[i] = s[*pointeur_i];
+		dst[i + 1] = s[*pointeur_i + 1];
+		dst[i + 2] = s[*pointeur_i + 2];
+		dst[i + 3] = s[*pointeur_i + 3];
+		dst[i + 4] = s[*pointeur_i + 4];
+		dst[i + 5] = s[*pointeur_i + 5];
+		dst[i + 6] = s[*pointeur_i + 6];
+		dst[i + 7] = s[*pointeur_i + 7];
+		i += 8;
+		*pointeur_i += 8;
+	}
+}
+
 static char		*take_word_in_str(size_t *pointeur_i, char const *s, char c)
 {
 	size_t	len;
@@ -56,12 +106,15 @@ static char		*take_word_in_str(size_t *pointeur_i, char const *s, char c)
 	new_str = ft_strnew(len - *pointeur_i);
 	if (new_str)
 	{
+		ft_strsplit_bulk16(&len, pointeur_i, s, new_str);
+		ft_strsplit_bulk8(&len, pointeur_i, s, new_str);
 		while (*pointeur_i < len)
 		{
 			new_str[i] = s[*pointeur_i];
 			i++;
 			*pointeur_i += 1;
 		}
+
 		return (new_str);
 	}
 	return (0);
