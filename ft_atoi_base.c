855fbb9f70201f4af7543b94ea3e787140895cb5
Jean-baptiste MARSAL <jmarsal@student.42.fr>
2016-06-27 16:23:56 +0200

    push

--- ft_atoi_base.c
+++ ft_atoi_base.c
@@ -6,42 +6,50 @@
 /*   By: jmarsal <jmarsal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/03 15:03:42 by jmarsal           #+#    #+#             */
-/*   Updated: 2016/06/26 16:04:16 by jmarsal          ###   ########.fr       */
+/*   Updated: 2016/06/27 11:05:36 by jmarsal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int     ft_atoi_base(char *str, int str_base)
+static int	get_uc(char c)
 {
-    int i;
-    int power;
-    int uc;
-    int result;
+	int uc;
 
-    i = ft_strlen(str) - 1;
-    power = 1;
-    result = 0;
 	uc = 0;
-    while (i >= 0)
-    {
-        if (str[i] == 'F' || str[i] == 'f')
-            uc = 15;
-        else if (str[i] == 'E' || str[i] == 'e')
-            uc = 14;
-        else if (str[i] == 'D' || str[i] == 'd')
-            uc = 13;
-        else if (str[i] == 'C' || str[i] == 'c')
-            uc = 12;
-        else if (str[i] == 'B' || str[i] == 'b')
-            uc = 11;
-        else if (str[i] == 'A' || str[i] == 'a')
-            uc = 10;
-        else if (str[i] >= '0' && str[i] <= '9')
-            uc = str[i] - '0';
-        result = result + uc * power;
-        power = power * str_base;
-        i--;
-    }
-    return (result);
+	if (c == 'F' || c == 'f')
+		uc = 15;
+	else if (c == 'E' || c == 'e')
+		uc = 14;
+	else if (c == 'D' || c == 'd')
+		uc = 13;
+	else if (c == 'C' || c == 'c')
+		uc = 12;
+	else if (c == 'B' || c == 'b')
+		uc = 11;
+	else if (c == 'A' || c == 'a')
+		uc = 10;
+	else if (c >= '0' && c <= '9')
+		uc = c - '0';
+	return (uc);
+}
+
+int			ft_atoi_base(char *str, int str_base)
+{
+	int i;
+	int power;
+	int uc;
+	int result;
+
+	i = ft_strlen(str) - 1;
+	power = 1;
+	result = 0;
+	while (i >= 0)
+	{
+		uc = get_uc(str[i]);
+		result = result + uc * power;
+		power = power * str_base;
+		i--;
+	}
+	return (result);
 }
