d04d53ad45458fd5e84b7a199f10128901142595
jmarsal <jmarsal@student.42.fr>
2016-10-11 16:19:17 +0200

    MAJ mineur sur ft_atoi et ft_array_create

--- src_conversion/ft_atoi.c
+++ src_conversion/ft_atoi.c
@@ -6,34 +6,27 @@
 /*   By: jmarsal <jmarsal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/01 23:17:49 by jmarsal           #+#    #+#             */
-/*   Updated: 2016/01/12 18:23:30 by jmarsal          ###   ########.fr       */
+/*   Updated: 2016/10/11 15:20:17 by jmarsal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_atoi(const char *str)
 {
 	int	i;
-	int	n;
+	int	neg;
 	int	value;
 
 	i = 0;
-	n = 0;
 	value = 0;
 	while ((str[i] == ' ') || (str[i] == '\t') || (str[i] == '\n')
 			|| (str[i] == '\v') || (str[i] == '\f') || (str[i] == '\r'))
 		i++;
-	if (str[i] == 45)
-		n = 1;
-	if ((str[i] == 45) || (str[i] == 43))
+	neg = (str[i] == '-') ? 1 : 0;
+	if ((str[i] == '-') || (str[i] == '+'))
 		i++;
-	while (str[i] >= 48 && str[i] <= 57)
-	{
-		value *= 10;
-		value += ((int)str[i] - 48);
-		i++;
-	}
-	if (n == 1)
-		return (-value);
-	else
-		return (value);
+	while (ft_isdigit(str[i]))
+		value = value * 10 + (str[i++] - '0');
+	return (neg ? -value : value);
 }
